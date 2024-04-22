16457276a90d8d0121fff8d559078ae73ab84dd6
lamiyarangwala <lkr9547@nyu.edu>
2024-04-22 14:05:31 -0400

    updates to alu and aludec

--- catalog/aludec/aludec.sv
+++ catalog/aludec/aludec.sv
@@ -20,10 +20,38 @@ module aludec
     //
     // ---------------- PORT DEFINITIONS ----------------
     //
+    input aluop,
+    input funct,
+    output alucontrol,
+    )
 
     //
     // ---------------- MODULE DESIGN IMPLEMENTATION ----------------
     //
+    always @ (aluop, funct) begin
+        casez (aluop)
+            00: alucontrol = 010;  // add
+            ?1: alucontrol = 110;  // sub
+            1?: begin
+                case (funct)
+                    000: alucontrol = 010 // add
+                    001: alucontrol = 110 // sub
+                    010: alucontrol = 000 // and
+                    011: alucontrol = 001 // or
+                    100: alucontrol = 111 // slt
+                    101: alucontrol = 010 // mult
+                    110: alucontrol = 110 // nand
+                    111: alucontrol = 000 // nor
+
+
+
+                endcase
+            end
+
+
+        endcase
+    end
+
 
 endmodule
 
