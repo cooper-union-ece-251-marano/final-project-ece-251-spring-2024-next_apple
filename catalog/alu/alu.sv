5b67e9a8f2f2a1b391cf17aed4d75438ecedd17a
lamiyarangwala <lkr9547@nyu.edu>
2024-04-27 18:23:28 -0400

    alu working

--- catalog/alu/alu.sv
+++ catalog/alu/alu.sv
@@ -16,23 +16,23 @@
 `timescale 1ns/100ps
 
 module alu
-    #(parameter n = 32)(
+    #(parameter n = 16)(
 
     // ---------------- PORT DEFINITIONS ----------------------------
     input [n-1:0] srcA, srcB,
     input [2:0] alucontrol,
-    output reg [n-1:0] out,
-    output cout);
+    output reg [n-1:0] out
+    );
 
     // ---------------- MODULE DESIGN IMPLEMENTATION ----------------
     always_comb begin
         case (alucontrol) 
-            3'b000: out = srcA && srcB;            // and
-            3'b001: out = srcA || srcB;            // or
+            3'b000: out = srcA & srcB;            // and
+            3'b001: out = srcA | srcB;            // or
             3'b010: out = srcA + srcB;             // add
-            3'b011: out = srcA << srcB;             //  sll
-            3'b100: out = !(srcA && srcB);         // nand
-            3'b101: out = !(srcA || srcB);         // nor
+            3'b011: out = srcA << srcB;            //  sll
+            3'b100: out = srcA ~& srcB;         // nand
+            3'b101: out = srcA ~| srcB;         // nor
             3'b110: out = srcA - srcB;             // sub
             3'b111: out = (srcA < srcB) ? 0 : 1;   // slt
         endcase
--- catalog/alu/tb_alu.sv
+++ catalog/alu/tb_alu.sv
@@ -19,8 +19,56 @@
 module tb_alu;
     parameter N = 16;
     reg [N-1:0] SRCA, SRCB;
-    reg ALUCONTROL;
-    
+    reg [2:0] ALUCONTROL;
+    logic [N-1:0] OUT;
+
+
+    initial begin
+        $dumpfile("alu.vcd");
+        $dumpvars(0, uut);
+        $monitor("srcA = %b, srcB = %b, alucontrol = %b, out = %b", SRCA, SRCB, ALUCONTROL, OUT);
+       
+    end
+
+    initial
+    begin: apply_stimulus
+      SRCA <= #N'b1000000000010010;
+      SRCB <= #N'b1010011100010010;
+      ALUCONTROL <= 3'b000;
+      #10
+      SRCA <= #N'b1000000000010010;
+      SRCB <= #N'b1010011100010010;
+      ALUCONTROL <= 3'b001;
+      #10
+      SRCA <= #N'b1000000000010010;
+      SRCB <= #N'b1010011100010010;
+      ALUCONTROL <= 3'b010;
+      #10
+      SRCA <= #N'b1000000000010010;
+      SRCB <= #N'b0000000000000010;
+      ALUCONTROL <= 3'b011;
+      #100
+      SRCA <= #N'b1000000000010010;
+      SRCB <= #N'b1010011100010010;
+      ALUCONTROL <= 3'b100;
+      #100
+      SRCA <= #N'b1000000000010010;
+      SRCB <= #N'b1010011100010010;
+      ALUCONTROL <= 3'b101;
+      #100
+      SRCA <= #N'b1000000000010010;
+      SRCB <= #N'b1010011100010010;
+      ALUCONTROL <= 3'b110;
+      #10
+      SRCA <= #N'b1000000000010010;
+      SRCB <= #N'b1010011100010010;
+      ALUCONTROL <= 3'b111;
+      
+   end
+
+   alu #(.n(N)) uut(
+        .srcA(SRCA), .srcB(SRCB), .alucontrol(ALUCONTROL), .out(OUT)
+   );
 
 endmodule
 `endif // TB_ALU
