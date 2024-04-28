d28e1e71f3dc15209f635a7deb3018dd6427acbe
lamiyarangwala <lkr9547@nyu.edu>
2024-04-28 16:18:17 -0400

    updated authors

--- catalog/clock/clock.sv
+++ catalog/clock/clock.sv
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // The Cooper Union
 // ECE 251 Spring 2024
-// Engineer: Prof Rob Marano
+// Engineer: Lamiya Rangwala, Dylan Meyer O'Connor
 // 
 //     Create Date: 2023-02-07
 //     Module Name: clock
