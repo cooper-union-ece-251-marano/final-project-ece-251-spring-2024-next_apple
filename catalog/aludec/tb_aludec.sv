9e37e56f6b98f252d0719fb284db72b18cb5696f
Dylan Meyer-O'Connor <dylansmo2003@gmail.com>
2024-05-01 19:57:55 -0400

    Working alu decoder + testbench

--- catalog/aludec/tb_aludec.sv
+++ catalog/aludec/tb_aludec.sv
@@ -1,9 +1,9 @@
 //////////////////////////////////////////////////////////////////////////////////
 // The Cooper Union
 // ECE 251 Spring 2024
-// Engineer: YOUR NAMES
+// Engineer: Dylan Meyer-O'Connor & Lamiya Rangwala
 // 
-//     Create Date: 2023-02-07
+//     Create Date: 4/30/2024
 //     Module Name: tb_aludec
 //     Description: Test bench for simple behavorial ALU decoder
 //
@@ -17,7 +17,41 @@
 `include "aludec.sv"
 
 module tb_aludec;
-    parameter n = 32;
+    parameter n = 16;
+	
+    logic [2:0] Funct, AluCon;
+    logic [1:0] AluOp;
 
+
+    initial begin
+    	$dumpfile("aludec.vcd");
+	$dumpvars(0, uut);
+        //$monitor("enable = %b clk = %b", enable, clk);
+        $monitor("time=%0t \t aluop=%b funct=%b alucontrol=%h ",$realtime,AluOp,Funct,AluCon);
+	
+    end
+
+    initial begin 
+	#10 AluOp <= 2'b00;
+	#10 Funct <= 3'b000;
+	#10 Funct <= 3'b111;
+	#10 AluOp <= 2'b01; 
+	#10 Funct <= 3'b000;
+	#10 AluOp <= 2'b11;
+	#10 Funct <= 001;
+	#10 Funct <= 010;
+	#10 Funct <= 011;
+	#10 Funct <= 100;
+
+	
+	
+    end 
+	
+
+
+
+    aludec uut(
+	.aluop(AluOp), .funct(Funct), .alucontrol(AluCon)
+    );
 endmodule
 `endif // TB_ALUDEC
