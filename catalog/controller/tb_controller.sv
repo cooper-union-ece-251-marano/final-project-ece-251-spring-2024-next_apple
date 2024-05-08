e1d9f5a220c061a7db49778f21f28da6dd360856
lamiyarangwala <lkr9547@nyu.edu>
2024-05-08 15:36:23 -0400

    added names

--- catalog/controller/tb_controller.sv
+++ catalog/controller/tb_controller.sv
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // The Cooper Union
 // ECE 251 Spring 2024
-// Engineer: YOUR NAMES
+// Engineer: Lamiya Rangwala, Dylan Meyer O'Connor
 // 
 //     Create Date: 2023-02-07
 //     Module Name: tb_controller
